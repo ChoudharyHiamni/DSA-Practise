432eb5dd21ffb6629209477b90614c3a7e18633d
Himani Choudhary <143154296+ChoudharyHiamni@users.noreply.github.com>
2025-02-26 12:47:45 +0530

    Create RotateArray.java

---
+++ RotateArray.java
@@ -0,0 +1,30 @@
+public class rotatearray {
+    public static void rotate(int nums[], int k) {
+        int n = nums.length;
+        k = k % n;
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+
+    public static void reverse(int nums[], int left, int right) {
+        while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String args[]) {
+        int nums[] = {1, 2, 3, 4, 5, 6, 7};
+        int k = 3;
+        rotate(nums, k);
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i] + "");
+        }
+    }
+}
+
+// Time Complexity: O(n) where n is the number of elements in input array.
