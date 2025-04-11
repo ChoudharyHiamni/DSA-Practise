d00b467d7f8b263bb25251d3e4ed285e99c347c7
Himani Choudhary <143154296+ChoudharyHiamni@users.noreply.github.com>
2025-04-11 10:50:16 +0530

    Create treeSet.java

---
+++ treeSet.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class treeSet {
+    
+    public static  void main(String args[]){
+        TreeSet<String> ts=new TreeSet<>();
+        ts.add("Delhi");
+        ts.add("Mumbai");
+        ts.add("Banglore");
+        System.out.println(ts); //in ascending or sorted order
+    }
+}
