fc509e238c6483a1bbda08f5422fdd9fcab11a39
Himani Choudhary <143154296+ChoudharyHiamni@users.noreply.github.com>
2025-04-11 10:49:34 +0530

    Create linkedHashSet.java

---
+++ linkedHashSet.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class linkedHashSet {
+    
+    public static void main(String args[]){
+         LinkedHashSet<String> lhs=new LinkedHashSet<>();
+         lhs.add("Delhi");
+         lhs.add("Mumbai");
+         lhs.add("Banglore");
+         System.out.println(lhs);
+    }
+}
